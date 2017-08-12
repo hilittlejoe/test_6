5be035205483f1d992c0d500578aac0c92669655
hilittlejoe <xiaojun000lee@gmail.com>
2017-08-12 20:51:54 +0800

    add new

--- README.md
+++ README.md
@@ -1,19 +1,18 @@
 ## 网站性能优化项目
 
-### 说明
-该性能优化项目直接下载到本地即可加载运行，并未使用构建工具。
+### 如何运行本应用
+直接将库下载到本地然后用浏览器打开index.html即可运行，或者，你也可以访问本项目的 [GitHub Pages](https://yuliangji.github.io/WebsiteOptimization/index.html) 来运行。
 
-#### Part 1: 优化 index.html 的 PageSpeed Insights 得分
-1. 为 print.css 添加了 media="print", 按需加载。
-2. 为 analytics.js 添加了 async 属性, 异步加载。
-3. 将 style.css 内嵌, 缩短了关键渲染路径长度。 
-4. 对网页中引用的图片进行了压缩和裁剪。
-5. Google Fonts变为异步加载，通过添加 [ Web Font Loader ](https://www.lockedowndesign.com/load-google-fonts-asynchronously-for-page-speed/)来实现。
+### 对 index.html 进行的优化概述
+* 将 print.css和Google Fonts css变为异步加载，通过添加 media:print 属性 和[ Web Font Loader ](https://www.lockedowndesign.com/load-google-fonts-asynchronously-for-page-speed/)来实现。
+* 将 style.css 嵌入 index.html 以缩短关键渲染路径长度。
+* 对 index.html 中引用的图片进行了压缩。
 
-#### Part 2: 优化 pizza.html 的 FPS（每秒帧数）
+最终[Google PageSpeed Insights 评分] 94 移动设备/96 桌面设备。
 
-1. 优化了项目中一些for循环，避免重复再DOM查找操作，已在 main.js 中注释
-2. 541行： 为 elem 添加了 will-change CSS 属性，避免图层重绘制。
-3. 545行： 使用了 requestAnimationFrame 优化绘制动画操作。
-
-
+### 对 pizza.html 的 FPS （每秒帧数）进行的优化概述
+* 参考[避免强制同步布局](https://developers.google.com/web/fundamentals/performance/rendering/avoid-large-complex-layouts-and-layout-thrashing#_2)对 changePizzaSizes
+  函数进行了重构，先读取样式而后执行更改以避免强制同步布局问题。
+* 使用 requestAnimationFrame 优化 updatePositions 中的绘制动画操作。
+* 为披萨滑窗增加了 will-change CSS 属性，这样每一个 pizza 都会有自己的图层，可以避免图层重绘制。
+* 最终在 Chrome 开发工具的 Rendering-FPS Meter 中滚动页面可以看到fps稳定在60。
--- views/js/main.js
+++ views/js/main.js
@@ -403,13 +403,13 @@ var resizePizzas = function(size) {
   function changeSliderLabel(size) {
     switch(size) {
       case "1":
-        document.querySelector("#pizzaSize").innerHTML = "Small";
+        document.getElementById("pizzaSize").innerHTML = "Small";
         return;
       case "2":
-        document.querySelector("#pizzaSize").innerHTML = "Medium";
+        document.getElementById("pizzaSize").innerHTML = "Medium";
         return;
       case "3":
-        document.querySelector("#pizzaSize").innerHTML = "Large";
+        document.getElementById("pizzaSize").innerHTML = "Large";
         return;
       default:
         console.log("bug in changeSliderLabel");
@@ -419,39 +419,28 @@ var resizePizzas = function(size) {
   changeSliderLabel(size);
 
    // 返回不同的尺寸以将披萨元素由一个尺寸改成另一个尺寸。由changePizzaSlices(size)函数调用
-  function determineDx (elem, size) {
-    var oldWidth = elem.offsetWidth;
-    var windowWidth = document.querySelector("#randomPizzas").offsetWidth;
-    var oldSize = oldWidth / windowWidth;
-
-    // 将值转成百分比宽度
-    function sizeSwitcher (size) {
+  function determineDx (size) {
       switch(size) {
         case "1":
-          return 0.25;
+          return 25;
         case "2":
-          return 0.3333;
+          return 33.33;
         case "3":
-          return 0.5;
+          return 50;
         default:
-          console.log("bug in sizeSwitcher");
+          console.log("bug in determineDx");
       }
-    }
-
-    var newSize = sizeSwitcher(size);
-    var dx = (newSize - oldSize) * windowWidth;
-
-    return dx;
   }
 
   // 遍历披萨的元素并改变它们的宽度
-  // pizza的尺寸都是一样的,所以只要获取一次就足够了，避免在for循环重复执行查找
   function changePizzaSizes(size) {
-    var randomPizza = document.querySelectorAll(".randomPizzaContainer");
-    var dx = determineDx(randomPizza[0], size);
-    var newwidth = randomPizza[0].offsetWidth + dx + 'px';
-    for (var i = 0; i < randomPizza.length; i++) {
-      randomPizza[i].style.width = newwidth;
+      //原来的changePizzaSize函数会在for循环中重新多次读取元素的offsetwidth，
+      //这会引起样式更改和重新布局，由于每个PizzaContainer的offsetwidth实际上是一样的，
+      //因此这是不必要的读取，故将其删去并简化。
+      var dx = determineDx(size);
+      var objs = document.querySelectorAll(".randomPizzaContainer");
+    for (var i = 0; i < objs.length; i++) {
+      objs[i].style.width = dx + '%';
     }
   }
 
@@ -467,9 +456,8 @@ var resizePizzas = function(size) {
 window.performance.mark("mark_start_generating"); // 收集timing数据
 
 // 这个for循环在页面加载时创建并插入了所有的披萨
-// 定义了pizzasDiv，避免在for循环重复执行查找
-var pizzasDiv = document.getElementById("randomPizzas");
 for (var i = 2; i < 100; i++) {
+  var pizzasDiv = document.getElementById("randomPizzas");
   pizzasDiv.appendChild(pizzaElementGenerator(i));
 }
 
@@ -501,15 +489,8 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  // 定义了 items，避免在for循环重复执行查找
-  // 定义了 pha 来存储 scrollTop
-  var items = document.querySelectorAll('.mover');
-  var lengths = items.length;
-  var pha = document.body.scrollTop / 1250;
-  for (var i = 0; i < lengths; i++) {
-    var phase = Math.sin( pha + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
-  }
+  //使用requestAnimationFrame来优化动画，动画绘制操作都转移进新增的render函数中
+  window.requestAnimationFrame(render);
 
   // 再次使用User Timing API。这很值得学习
   // 能够很容易地自定义测量维度
@@ -520,6 +501,16 @@ function updatePositions() {
     logAverageFrame(timesToUpdatePosition);
   }
 }
+//基本上实现了原来的updatePositions函数的功能，将一些操作从循环中移出以优化性能，
+//另外，用transform代替了left以避免强制同步布局
+function render(){
+    var items = document.querySelectorAll('.mover');
+    var p = document.body.scrollTop / 1250;
+    for (var i = 0; i < items.length; i++) {
+      var phase = Math.sin( p + (i % 5));
+      items[i].style.transform = "translateX("+ (100 * phase) +"px)";
+    }
+};
 
 // 在页面滚动时运行updatePositions函数
 window.addEventListener('scroll', updatePositions);
@@ -528,9 +519,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  // 定义了movingPizzas，避免在for循环重复执行查找
-  var movingPizzas = document.querySelector("#movingPizzas1");
-  for (var i = 0; i < 200; i++) {
+  for (var i = 0; i < 100; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
@@ -538,10 +527,11 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    // 添加了 will-change 属性，避免图层重绘制
-    elem.style.willChange = 'transform';
-    movingPizzas.appendChild(elem);
+    //由于更改了移动pizza的css属性为transform故在这里初始化left
+    //给pizza增加will-change属性来避免图层重绘制
+    elem.style.left = elem.basicLeft + 'px';
+    elem.style['will-change'] = "transform";
+    document.getElementById("movingPizzas1").appendChild(elem);
   }
-  // 使用了 requestAnimationFrame，优化绘制动画操作
-  requestAnimationFrame(updatePositions);
+  updatePositions();
 });
