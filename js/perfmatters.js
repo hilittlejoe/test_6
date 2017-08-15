206dda1f63e1d4535377e7f19f3455e1f21e5893
hilittlejoe <xiaojun000lee@gmail.com>
2017-08-15 13:43:02 +0800

    add new

--- js/perfmatters.js
+++ js/perfmatters.js
@@ -1,1 +1,3 @@
-function logCRP(){var t=window.performance.timing,dcl=t.domContentLoadedEventStart-t.domLoading,complete=t.domComplete-t.domLoading;var stats=document.getElementById("crp-stats");stats.textContent='DCL: '+dcl+'ms, onload: '+complete+'ms';}window.addEventListener("load",function(event){logCRP();});
+// Measuring the Critical Rendering Path with Navigation Timing
+// https://developers.google.com/web/fundamentals/performance/critical-rendering-path/measure-crp
+logCRP() { var t = window.performance.timing, dcl = t.domContentLoadedEventStart - t.domLoading, complete = t.domComplete - t.domLoading; var stats = document.getElementById("crp-stats"); stats.textContent = 'DCL: ' + dcl + 'ms, onload: ' + complete + 'ms';}window.addEventListener("load", function(event) { logCRP();});
