95aff3a5e27c12c8b7756dca7c2bef5845207dd3
hilittlejoe <xiaojun000lee@gmail.com>
2017-11-10 20:56:38 +0800

    add new

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,71 @@
-var gulp = require('gulp'),
-uglify = require("gulp-uglify"),
-minifyCss = require("gulp-minify-css"),
-minifyHtml = require("gulp-minify-html"),
-imagemin = require('gulp-imagemin');
+var gulp = require('gulp');
+var gulpLoadPlugins = require('gulp-load-plugins');
+var $ = gulpLoadPlugins();
+var pngquant = require('imagemin-pngquant');
 
-
-gulp.task('default', function() {
-    gulp.src('src/**').pipe(gulp.dest('dest/'));
+gulp.task('clean', function() {
+    return gulp.src(['dist'], { read: false })
+        .pipe($.rimraf({ force: true }));
 });
 
-gulp.task('minify-js', function () {
-    gulp.src('src/**/*.js') // 要压缩的js文件
-    .pipe(uglify())  //使用uglify进行压缩,更多配置请参考：
-    .pipe(gulp.dest('dest/')); //压缩后的路径
+gulp.task('image', ['clean'], function() {
+    return gulp.src('img/*')
+        .pipe($.cache($.imagemin({
+            progressive: true,
+            svgoPlugins: [{ removeViewBox: false }],
+            use: [pngquant()]
+        })))
+        .pipe(gulp.dest('dist/img'));
 });
 
-gulp.task('minify-css', function () {
-    gulp.src('src/**/*.css') // 要压缩的css文件
-    .pipe(minifyCss()) //压缩css
-    .pipe(gulp.dest('dest/'));
+gulp.task('image2', ['clean'], function() {
+    return gulp.src('views/images/*')
+        .pipe($.cache($.imagemin({
+            progressive: true,
+            svgoPlugins: [{ removeViewBox: false }],
+            use: [pngquant()]
+        })))
+        .pipe(gulp.dest('dist/views/images'));
 });
 
-gulp.task('minify-html', function () {
-    gulp.src('src/**/*.html') // 要压缩的html文件
-    .pipe(minifyHtml()) //压缩
-    .pipe(gulp.dest('dest/'));
+gulp.task('css', ['clean'], function() {
+    return gulp.src('css/*')
+        .pipe($.autoprefixer({ remove: false }))
+        .pipe($.cssmin({ advanced: false }))
+        .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('imagemin', function () {    
-	gulp.src('src/img/*')
-		.pipe(imagemin([
-	                      imagemin.gifsicle({interlaced: true}),
-	                      imagemin.jpegtran({progressive: true}),
-	                      imagemin.optipng({optimizationLevel: 5}),
-						  imagemin.svgo({
-								plugins: [
-									{removeViewBox: true},
-									{cleanupIDs: false}
-								]
-						  })
-	                    ]))
-		.pipe(gulp.dest('dest/img')) 
+gulp.task('css2', ['clean'], function() {
+    return gulp.src('views/css/*')
+        .pipe($.autoprefixer({ remove: false }))
+        .pipe($.cssmin({ advanced: false }))
+        .pipe(gulp.dest('dist/views/css'));
 });
 
-var imageop = require('gulp-image-optimization');
- 
-gulp.task('imageopti', function() {
-    gulp.src(['src/**/*.png','src/**/*.jpg','src/**/*.gif','src/**/*.jpeg']).pipe(imageop({
-        optimizationLevel: 5,
-        progressive: true,
-        interlaced: true
-    })).pipe(gulp.dest('dest/'));
+gulp.task('js', ['clean'], function() {
+    return gulp.src('js/*.js')
+        .pipe($.uglify())
+        .pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('watch', function () {
-    gulp.watch(['src/js/*.js','src/css/*.css','src/*.html'], ['minify-js','minify-css','minify-html']);
+gulp.task('js2', ['clean'], function() {
+    return gulp.src('views/js/*.js')
+        .pipe($.uglify())
+        .pipe(gulp.dest('dist/views/js'));
 });
 
+gulp.task('html', ['clean'], function() {
+    return gulp.src('*.html')
+        .pipe($.processhtml())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('html2', ['clean'], function() {
+    return gulp.src('views/*.html')
+        .pipe($.processhtml())
+        .pipe(gulp.dest('dist/views'));
+});
+
+gulp.task('default', [], function() {
+    gulp.run('image', 'image2', 'css', 'css2', 'js', 'js2', 'html', 'html2');
+});
